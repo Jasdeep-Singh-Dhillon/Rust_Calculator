0cf92e61986d140e2ba1969f87cee584f0f37c2c
Jasdeep Singh Dhillon <jdps2867@gmail.com>
2022-06-24 16:32:50 -0700

    Implemented integer input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::Write;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input("Enter integer: ");
+
+    match input {
+        Some(n) => println!("You Entered {}", n),
+        None => {}
+    }
+}
+
+fn get_input(prompt: &str) -> Option<i32> {
+    print!("{}", prompt);
+    let _ = std::io::stdout().flush();
+    let mut input: String = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read from stdin");
+    
+    let trimmed = input.trim();
+    
+    match trimmed.parse::<i32>() {
+        Ok(n) => Some(n),
+        Err(..) => {
+            println!("Please enter an integer, {} is not an Integer", trimmed);
+            None
+        }
+    }
 }
